4c321b90eb02f36199fccb1e964d004a52749310
ut01107 <ut01107@UNICOMTIC11>
2024-10-19 17:07:46 +0530

    first commit

---
+++ TaskManager/Data/TaskContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Data
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
